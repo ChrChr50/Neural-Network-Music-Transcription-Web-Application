a451edff8cdaf925252ebfcb365013f4c0d67f2d
Chris Choilee <91154952+ChrChr50@users.noreply.github.com>
2022-07-27 03:22:43 -0600

    Delete mysite directory

--- mysite/music/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-
-class AudioForm(forms.Form):
-    docfile = forms.FileField(
-        label = 'Select a file',
-        help_text = 'File must be .wav, .ogg, .mp3, or .mp4'
-    )
--- mysite/music/models.py
+++
@@ -1,4 +0,0 @@
-from django.db import models
-
-class Audio(models.Model):
-    docfile = models.FileField(upload_to = 'documents/%Y/%m/%d')
