f0eeda592c9014e76fb3faa06090e92c17d826ee
Chris Choilee <91154952+ChrChr50@users.noreply.github.com>
2022-08-10 18:11:39 -0600

    Update settings.py

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -123,3 +123,5 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+FILE_UPLOAD_HANDLERS = ['django.core.files.uploadhandler.TemporaryFileUploadHandler']
