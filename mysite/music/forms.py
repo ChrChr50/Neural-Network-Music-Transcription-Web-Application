5e0155626b5ef5229c868992cfaee21df3901b30
Chris Choilee <91154952+ChrChr50@users.noreply.github.com>
2022-08-10 18:09:58 -0600

    Update forms.py

--- mysite/music/forms.py
+++ mysite/music/forms.py
@@ -3,5 +3,6 @@ from django import forms
 class AudioForm(forms.Form):
     docfile = forms.FileField(
         label = 'Select a file',
-        help_text = 'File must be .wav, .ogg, or .mp3'
+        help_text = 'File must be .wav, .ogg, or .mp3',
+        widget = forms.FileInput(attrs = {'class': "form-control center"})
     )
