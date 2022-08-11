3b5c21c34c1465c1285c7f2283ecc574cfe14649
Chris Choilee <91154952+ChrChr50@users.noreply.github.com>
2022-08-10 18:10:56 -0600

    Update urls.py

--- mysite/music/urls.py
+++ mysite/music/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.FileHandler, name = 'FileHandler')
+    path('', views.FileHandler, name = 'FileHandler'),
+    path('confirmation/', views.Confirmation, name = 'Confirmation')
 ]
